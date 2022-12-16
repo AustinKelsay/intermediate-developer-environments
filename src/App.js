91cd7fda2ae17f008686c0a6ade1fb4895a57921
austinkelsay <austinkelsay@yahoo.com>
2022-12-16 14:45:48 -0600

    Testing secret

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  console.log(process.env);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>{process.env.REACT_APP_SECRET}</h1>
       </header>
     </div>
   );
